24d7b4f78623bbd58a3bab964202a25aeb8bdc44
Spirregotv <martin14814@gmail.com>
2025-02-05 19:56:02 -0600

    Creacion del formulario

--- Proyecto_TDPII/Controllers/HomeController.cs
+++ Proyecto_TDPII/Controllers/HomeController.cs
@@ -17,7 +17,10 @@ namespace Proyecto_TDPII.Controllers
         {
             return View();
         }
-
+        public IActionResult Crear_evento()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
