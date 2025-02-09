e20f67ef84a23db618025916c814086adc780cb9
humbertobr2711 <humbertoasabarajas2711@gmail.com>
2025-02-08 19:07:34 -0600

    el contenido de la pagina principal se cambio a un archivo distinto, para que este pudiera mostrarse se forma dinamica, se agrego una funcion dentro del controlador para que pudiera hacer este cambio de contenido, el problema es que los estilos no los esta queriendo implementar.

--- Proyecto_TDPII/Controllers/HomeController.cs
+++ Proyecto_TDPII/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace Proyecto_TDPII.Controllers
             return View();
         }
 
+        public IActionResult Enero()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
