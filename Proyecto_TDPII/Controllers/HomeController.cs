b3820d04da29030b7dea3846fcedc3f82d13f702
humbertobr2711 <humbertoasabarajas2711@gmail.com>
2025-02-08 20:27:44 -0600

    Ya se agrega el estilo a lo que son dos archivos del calendario (enero y febrero), agregar los demás para poder hacer las funciones en el controlador y funcione, tambien si pueden arreglar el problema de que el calendario sobrepasa los limites del menu de arriba

--- Proyecto_TDPII/Controllers/HomeController.cs
+++ Proyecto_TDPII/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace Proyecto_TDPII.Controllers
             return View();
         }
 
+        public IActionResult Febrero()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
