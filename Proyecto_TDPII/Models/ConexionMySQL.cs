0d2e2043faab9c49978c98728a984939cf4c2e2f
humbertobr2711 <humbertoasabarajas2711@gmail.com>
2025-02-09 14:04:00 -0600

    conexión con base de datos y mediante el comenado select mostrar el nomber de los registros

---
+++ Proyecto_TDPII/Models/ConexionMySQL.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+
+namespace Proyecto_TDPII.Models
+{
+    public class ConexionMySQL
+    {
+        private readonly string _cadenaConexion;
+
+        public ConexionMySQL(IConfiguration configuration)
+        {
+            _cadenaConexion = configuration.GetConnectionString("MiConexionMySQL");
+        }
+
+        public List<string> ObtenerUsuarios()
+        {
+            List<string> usuario = new List<string>();
+
+            using (MySqlConnection conexion = new MySqlConnection(_cadenaConexion))
+            {
+                conexion.Open();
+                string query = "SELECT Nombre FROM usuario"; 
+                MySqlCommand comando = new MySqlCommand(query, conexion);
+
+                using (MySqlDataReader reader = comando.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        usuario.Add(reader["nombre"].ToString());
+                    }
+                }
+            }
+
+            return usuario;
+        }
+    }
+}
--- Proyecto_TDPII/Program.cs
+++ Proyecto_TDPII/Program.cs
@@ -1,7 +1,10 @@
+using Proyecto_TDPII.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<ConexionMySQL>();
 
 var app = builder.Build();
 
