4a56bb93beb884673bbe5ab3947388e49950f58c
chavezz31 <emilianoalferez91@gmail.com>
2025-02-11 12:15:12 -0600

    se grego la opción de semana en donde muestra la semana del mes de febrero, se implemento de una forma no tan optima, pero es para empezar a darnos una idea

--- Proyecto_TDPII/Controllers/HomeController.cs
+++ Proyecto_TDPII/Controllers/HomeController.cs
@@ -97,6 +97,11 @@ namespace Proyecto_TDPII.Controllers
             return View();
         }
 
+        public IActionResult semanaaa()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
